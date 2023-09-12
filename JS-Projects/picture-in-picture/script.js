8b43421126d3f8192f4af5d56470d0cb443364e5
Vamshidhar Thonti <t.vamshidhar333@gmail.com>
2023-09-12 09:50:25 +0530

    Picture in Picture project

---
+++ JS-Projects/picture-in-picture/script.js
@@ -0,0 +1,56 @@
+const videoElement = document.querySelector("#video");
+const btnContainer = document.querySelector(".btn-container");
+const btn = document.querySelector("#button");
+
+// Prompt to select a media stream, pass to video element, then play
+const selectMediaStream = async function () {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
+    videoElement.srcObject = mediaStream;
+
+    videoElement.onloadedmetadata = async () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+// selectMediaStream();
+
+btn.addEventListener("click", async () => {
+  btn.disabled = true;
+
+  videoElement.requestPictureInPicture();
+
+  btn.disabled = false;
+});
+
+// const selectMediaStream = async function () {
+//   try {
+//     const mediaStream = await navigator.mediaDevices.getUserMedia({
+//       video: true,
+//     });
+//     videoElement.srcObject = mediaStream;
+
+//     videoElement.onloadedmetadata = async () => {
+//       videoElement.play();
+
+//       if (!document.pictureInPictureElement) {
+//         // Start PIP
+//         btn.textContent = "Stop";
+//         await videoElement.requestPictureInPicture();
+//       } else {
+//         btn.textContent = "Start";
+//         document.exitPictureInPicture();
+//       }
+//     };
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// };
+// btn.addEventListener("click", selectMediaStream);
+
+// selectMediaStream();
